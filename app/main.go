34c34748a7f97c0e3e23c2dcd536cffb4bc92230
Andrei Stanimir <andreistanimir@live.com>
2025-12-13 22:16:06 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -9,13 +9,24 @@ import (
 
 func main() {
 	for true {
-		fmt.Fprint(os.Stdout, "$ ")
-		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		command = strings.TrimRight(command, "\n")
+		_, err := fmt.Fprint(os.Stdout, "$ ")
+		if err != nil {
+			return
+		}
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		words := strings.Split(strings.TrimSpace(input), " ")
+		// command = strings.TrimRight(command, "\n")
+		command := words[0]
+		// fmt.Fprintf(os.Stderr, "You entered: %s\n", command)
+		rest := words[1:]
 		if command == "exit" {
 			return
 		}
-		fmt.Fprintf(os.Stdout, "%s: command not found", command)
-		fmt.Fprint(os.Stdout, "\n")
+		if command == "echo" {
+			fmt.Fprintf(os.Stdout, "%s\n", strings.Join(rest, " "))
+		} else {
+			fmt.Fprintf(os.Stdout, "%s: command not found", command)
+			fmt.Fprint(os.Stdout, "\n")
+		}
 	}
 }
